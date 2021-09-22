fd79ebced7a0ce303e42e12cf91dc9b8769cfb2c
li.bin <li.bin@mail.qubit-tech.com>
2021-09-22 17:00:22 +0800

    init

---
+++ aoi/build.bat
@@ -0,0 +1,6 @@
+REM npm install -g gitbook-cli
+REM CD C:\Users\bionl\AppData\Roaming\npm\node_modules\gitbook-cli\node_modules\npm\node_modules
+REM npm install graceful-fs@4.2.0 --save
+REM https://stackoverflow.com/questions/64211386/gitbook-cli-install-error-typeerror-cb-apply-is-not-a-function-inside-graceful
+gitbook build --gitbook=3.2.3
+pause
